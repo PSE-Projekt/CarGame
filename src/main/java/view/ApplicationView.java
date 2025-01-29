7a53017f9d24424e7b24ed4be9b69ffcc9a5763c
malik <malik.aljazzar@gmail.com>
2025-01-29 17:33:07 +0100

    implemented first attempt of APIHandler and ApplicationView

--- src/main/java/view/ApplicationView.java
+++ src/main/java/view/ApplicationView.java
@@ -3,15 +3,33 @@ package view;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.Map;
+
 public class ApplicationView extends Application {
+    private final APIHandler apiHandler;
+    private final Map<GameState, CustomScene> sceneMap;
+    private CustomScene currentScene;
+    private Stage stage;
+
+    public ApplicationView(GameInstanceAPI gameInstanceAPI, GameStateAPI gameStateAPI, PlayerAPI playerAPI) {
+        this.apiHandler = new APIHandler(gameInstanceAPI, gameStateAPI, playerAPI);
+        try {
+            stage = new Stage();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World");
-        primaryStage.show();
+        this.stage = primaryStage;
+        this.stage.setTitle("Car Game");
+        this.stage.show();
     }
 
     public void render() {
-        // meant to be called from the controller
+        currentScene = sceneMap.get(apiHandler.getGameStateAPI().getCurrentGameState());
+        this.stage.setScene(currentScene);
+        currentScene.render();
     }
 }
--- src/main/java/view/CustomScene.java
+++ src/main/java/view/CustomScene.java
@@ -3,10 +3,14 @@ package view;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 
 public abstract class CustomScene extends Scene {
-    public CustomScene(Parent root) {
-        super(root);
+    private final APIHandler apiHandler;
+
+    public CustomScene(APIHandler apiHandler) {
+        super(new VBox());
+        this.apiHandler = apiHandler;
     }
 
     public abstract void render();
