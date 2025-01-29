9d59a3dbb969809440317dfab7b1416a4ba69b8d
malik <malik.aljazzar@gmail.com>
2025-01-29 17:17:41 +0100

    implemented abstract CustomScene

--- src/main/java/view/CustomScene.java
+++ src/main/java/view/CustomScene.java
@@ -1,12 +1,13 @@
 package view;
 
 
-public abstract class CustomScene extends Scene {
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
+public abstract class CustomScene extends Scene {
+    public CustomScene(Parent root) {
+        super(root);
+    }
 
     public abstract void render();
-
-    public void switchScenes(){
-
-    }
 }
